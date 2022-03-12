cab002871a1a8aadc2cb8c150c7884317dabf821
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-03-13 02:29:16 +0600

    Group Todo done (crud) done

--- src/components/GroupTodo.js
+++ src/components/GroupTodo.js
@@ -4,6 +4,7 @@ import { HiX } from "react-icons/hi";
 
 const GroupTodo = ({ todoGroups, setTodoGroups }) => {
     const [isAddGroup, setIsAddGroup] = useState(false);
+    const [editGroupTodo, setEditGroupTodo] = useState(0);
 
     const handleGrpTodoSubmit = (e) => {
         e.preventDefault();
@@ -11,8 +12,21 @@ const GroupTodo = ({ todoGroups, setTodoGroups }) => {
 
         setTodoGroups([...todoGroups, { id: (new Date()).getTime(), grpTitle: grpTitleValue, todoList: [] }]);
 
-
         e.target.reset();
+    }
+
+    const handleDeleteGroupTodo = (id) => {
+        const restTodoGroups = todoGroups.filter(todo => todo.id !== id);
+        setTodoGroups(restTodoGroups)
+    }
+    const handleEditGroupTodo = (e, id, list) => {
+        e.preventDefault();
+        const value = e.target.editGroupTodo.value;
+
+        const foundTodoIndex = todoGroups.findIndex(todo => todo.id === id);
+        todoGroups[foundTodoIndex] = { id: id, grpTitle: value, todoList: list };
+        setEditGroupTodo(0)
+        console.log(todoGroups)
     }
 
     return (
@@ -22,10 +36,24 @@ const GroupTodo = ({ todoGroups, setTodoGroups }) => {
                 todoGroups.map((todoGroup, grpI) => {
                     return (
                         <div key={grpI} className="grp_todo">
-                            <div className="grp_todo__title_container">
-                                <span className="grp_todo__title">{todoGroup.grpTitle}</span>
-                                <span className="grp_todo__length">{todoGroup.todoList?.length}</span>
-                            </div>
+                            <button onClick={(e) => handleDeleteGroupTodo(todoGroup?.id)} className="grp_todo__delete_btn"><HiX /> </button>
+                            {editGroupTodo === todoGroup.id ?
+
+                                // Update Group todo title
+                                <div className="grp_todo__form_container">
+                                    <form onSubmit={(e) => handleEditGroupTodo(e, todoGroup?.id, todoGroup?.todoList)} className="grp_todo__form">
+                                        <input type="text" name="editGroupTodo" className="grp_todo__form_input" placeholder="Update group name" defaultValue={todoGroup.grpTitle} required />
+                                        <button type="submit" className="grp_todo__form_btn">Update</button>
+                                        <button onClick={() => setEditGroupTodo(0)} className="grp_todo__form_btn">Cancel</button>
+                                    </form>
+                                </div>
+                                :
+                                <div onDoubleClick={(e) => setEditGroupTodo(todoGroup.id)} className="grp_todo__title_container">
+                                    <span className="grp_todo__title">{todoGroup.grpTitle}</span>
+                                    <span className="grp_todo__length">{todoGroup.todoList?.length}</span>
+                                </div>
+
+                            }
                             <button className="todo__add_btn">
                                 <FiPlus size="20px" />
                             </button>
@@ -34,22 +62,22 @@ const GroupTodo = ({ todoGroups, setTodoGroups }) => {
                 })
             }
 
-            {!isAddGroup ?
-                <button onClick={() => setIsAddGroup(true)} className="grp_todo__add_btn">
-                    <FiPlus size="20px" />
-                    <span>Add Todo Group </span>
-                </button>
-                :
-                <div className="grp_todo__form_container">
-                    <button onClick={() => setIsAddGroup(false)} className="grp_todo__delete_btn"><HiX /> </button>
-                    <form onSubmit={(e) => handleGrpTodoSubmit(e)} className="grp_todo__form">
-
-                        <input type="text" name="groupTodo" className="grp_todo__form_input" placeholder="Enter group name" required />
-                        <button type="submit" className="grp_todo__form_btn">Add</button>
-                    </form>
-                </div>
+            {
+                !isAddGroup ?
+                    <button onClick={() => setIsAddGroup(true)} className="grp_todo__add_btn">
+                        <FiPlus size="20px" />
+                        <span>Add Todo Group </span>
+                    </button>
+                    :
+                    <div className="grp_add__form_container">
+                        <button onClick={() => setIsAddGroup(false)} className="grp_todo__delete_btn"><HiX /> </button>
+                        <form onSubmit={(e) => handleGrpTodoSubmit(e)} className="grp_add__form">
+                            <input type="text" name="groupTodo" className="grp_add__form_input" placeholder="Enter group name" required />
+                            <button type="submit" className="grp_add__form_btn">Add</button>
+                        </form>
+                    </div>
             }
-        </div>
+        </div >
     );
 };
 
